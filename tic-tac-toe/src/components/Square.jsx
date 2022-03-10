6fb5b9fd178ffd8703415eeab7566589be783441
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-10 14:42:47 +0530

    feat: add tic-tac-toe game in react

---
+++ tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,11 @@
+import './common.css';
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+export default Square;
