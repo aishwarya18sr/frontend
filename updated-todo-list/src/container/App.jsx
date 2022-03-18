dc770cd05c4b3078b3925c5639c6779dce95278f
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-18 17:07:12 +0530

    fix: delete unwanted elements in route

--- updated-todo-list/src/container/App.jsx
+++ updated-todo-list/src/container/App.jsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import List from '../components/List/List';
 import Task from '../components/Task/Task';
 import CreateList from '../components/CreateList/CreateList';
 import CreateTask from '../components/CreateTask/CreateTask';
 import EditTask from '../components/EditTask/EditTask';
-import LISTS from '../constants/lists';
 import { LISTS_ROUTE, TASKS_ROUTE } from '../constants/routes';
 
 function App() {
-  const [listData, setListData] = useState(LISTS);
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,10 +19,7 @@ function App() {
           <Route
             path={`${LISTS_ROUTE}/create`}
             element={(
-              <CreateList
-                listData={listData}
-                setListData={setListData}
-              />
+              <CreateList />
 )}
           />
           <Route
@@ -35,20 +29,13 @@ function App() {
           <Route
             path={`${LISTS_ROUTE}/:listId${TASKS_ROUTE}/create`}
             element={(
-              <CreateTask
-                listData={listData}
-                setListData={setListData}
-              />
+              <CreateTask />
 )}
           />
           <Route
             path={`${LISTS_ROUTE}/:listId${TASKS_ROUTE}/:taskId/update`}
             element={(
-              <EditTask
-                title="Update Task"
-                listData={listData}
-                setListData={setListData}
-              />
+              <EditTask />
 )}
           />
           <Route path="*" element={<div>404 Error. Page not found</div>} />
